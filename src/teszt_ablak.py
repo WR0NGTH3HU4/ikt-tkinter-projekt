9415eec62e6de7a972b51baee70de652df0f11b3
Császár András <csaszarandras@turr.hu>
2022-06-01 11:23:48 +0200

    Objektumorientált ablakok

--- src/teszt_ablak.py
+++ src/teszt_ablak.py
@@ -2,80 +2,23 @@ from multiprocessing.pool import TERMINATE
 from tkinter import *
 from tkinter import messagebox
 from mode import Mode
+from Window import Window
 
-EREDMENY_TEXT = "Eredmeny: {}"
+class TesztAblak(Window):
 
-class TesztAblak:
+    POINTS = [0, 0, 0, 150, 150, 150, 150, 0]
 
-    def place_all(self):
-        self.calc_button.grid(row = 3, column = 1, sticky=E)
-
-        self.geometry_frame.grid(row = 0, column = 0)
-        self.field_frame.grid(row = 0, column = 0)
-        self.canv.grid(row = 0, column = 1, padx = 20, pady = 20)
-
-        self.result_label.grid(row = 1, column = 0, columnspan = 2, sticky=W)
-
-        self.tl.configure(padx = 10, pady = 10)
-
-        for i, vals in enumerate(self.fields.values()):
-            vals[0].grid(row = i, column = 0)
-
-        for i, vals in enumerate(self.fields.values()):
-            vals[1].grid(row = i, column = 1)
-
-
-    def calc(self):
-
-        try:
-            a = int(self.fields["a"][1].get())
-            b = int(self.fields["b"][1].get())
-            c = int(self.fields["c"][1].get())
-        except ValueError:
-            messagebox.showerror("Hiba", "Számokat adj meg!")
-            return
-
-        if a <= 0 or b <= 0 or c <= 0:
-            return messagebox.showerror("Hiba", "A szamok amiket megadtal ellent mondanak a fizika torvenyeinek!")
-        # }}
+    def __init__(self, tk, mode: Mode):
+        super().__init__(tk, mode)
         
-        # Calculating -{{
-        result = 0
-        if self.mode == Mode.KERULET:
-            result = a + b + c
-        elif self.mode == Mode.TERULET:
-            result = a * b * c
-        result = str(result)
-
-        # }}
-        
-        self.result_label.config(text=EREDMENY_TEXT.format(result))
-
-    def __init__(self, tk, mode: Mode) -> None:
-
-        # Config -{{
-        self.POINTS = [0, 150, 75, 0, 150, 150]
-        self.title = "Teszt"
-        # }}
-
-
-        self.mode = mode
-        self.tl = Toplevel(tk)
-        self.tl.title(self.title)
-        self.geometry_frame = Frame(self.tl)
-        self.field_frame = Frame(self.geometry_frame)
-
+    def define_fields(self):
         self.fields = {
             "a": (Label(self.field_frame, text = "A: "), Entry(self.field_frame)),
             "b": (Label(self.field_frame, text = "B: "), Entry(self.field_frame)),
-            "c": (Label(self.field_frame, text = "C: "), Entry(self.field_frame)),
         }
 
-        self.canv = Canvas(self.geometry_frame, width = 150, height = 150)
-        self.canv.create_polygon(self.POINTS, fill = "white", outline = "black")
+    def calc_K(self):
+        return 69
 
-        self.calc_button = Button(self.field_frame, text="Szamitas", command=self.calc)
-        self.result_label = Label(self.tl, text = EREDMENY_TEXT.format(""))
-
-        self.place_all()
-        self.tl.mainloop()
+    def calc_T(self):
+        return 3452
