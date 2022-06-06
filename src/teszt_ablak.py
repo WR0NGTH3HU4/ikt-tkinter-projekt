a531c3fa8eccce60b1596123a509c49b7aabd98e
csandrisTurr <csaszarandras@turr.hu>
2022-06-06 19:15:34 +0200

    olvass leirast - szamitasi mod alapu dinamikus init eljaras - fieldek kikapcsolasa - hibakezeles innentol fel-manualisan tortenik

--- src/teszt_ablak.py
+++ src/teszt_ablak.py
@@ -17,8 +17,15 @@ class TesztAblak(Window):
             "b": (Label(self.field_frame, text = "B: "), Entry(self.field_frame)),
         }
 
+    def on_init_K(self):
+        self.disable_fields(["a"])
+
     def calc_K(self):
+        if self.calc_is_error(["b"]): return
+
         return 69
 
     def calc_T(self):
+        if self.calc_is_error(["a", "b"]): return
+
         return 3452
